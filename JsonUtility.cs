be12b0f5d822d7c72abadaafcfd61e74aca60ca4
EKISUKE <yuuki622800@yahoo.co.jp>
2018-03-04 18:22:50 +0900

    JSON読み込み処理追加
    JSON読み込み処理追加
    .sln追加
    gameSetting.jsonを読み込み、送信を追加

---
+++ JsonUtility.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Text;
+using System.Runtime.Serialization.Json;
+
+/// <summary>
+/// JSON読み込みユーティリティ
+/// </summary>
+public static class JsonUtility
+{
+    public static string GetFileString(string filePath)
+    {
+        StreamReader file = new StreamReader(filePath, Encoding.UTF8);
+        return file.ReadToEnd();
+    }
+
+    public static T Deserialize<T>(string filePath)
+    {
+        string fileString = GetFileString(filePath);
+        using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(fileString)))
+        {
+            var serializer = new DataContractJsonSerializer(typeof(T));
+            return (T)serializer.ReadObject(stream);
+        }
+	}
+}
